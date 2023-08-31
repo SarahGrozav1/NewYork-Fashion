a52b298de2648d238f786921aebc452f3e49ac60
Sarah <grozavsarah@gmail.com>
2023-08-31 05:39:38 +0000

    Renamed .json file and changed in run.py file name in line 16

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
 ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file('package.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('new_york_fashion')
